7891d53c7427536b2f016f369bc4559bc11d6bf4
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-20 11:24:40 +0100

    Import episode.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { greet } from "./utils/greet";
+import episodes from "./episodes.json";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return 
 }
 
 export default App;
