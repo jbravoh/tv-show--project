266cba8cf5eb645bfcd8fd58a4f9f5cddebd6e06
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-20 11:33:31 +0100

    Create TVShows component and interface

---
+++ src/components/TvShows.tsx
@@ -0,0 +1,30 @@
+interface IEpisode {
+    id: number;
+    url: string;
+    name: string;
+    season: number;
+    number: number;
+    type: string;
+    airdate: string;
+    airtime: string;
+    airstamp: string;
+    runtime: number;
+    image: {
+      medium: string;
+      original: string;
+    };
+    summary: string;
+    _links: { self: { href: string } };
+  }
+
+
+
+function TvShows(props: IEpisode): JSX.Element {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default TvShows;
