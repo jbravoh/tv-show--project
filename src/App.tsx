fe0f2d8a1e83bcc8d5f9228ffcbc676323a2aa65
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-23 12:58:48 +0100

    Add AppHeader & MainContent to React component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import MainContent from "./components/MainContent";
+import AppHeader from "./components/AppHeader";
 import episodes from "./episodes.json";
+import "./css/App.css";
 
 function App(): JSX.Element {
-  return 
+  return (
+    <>
+      <AppHeader />
+      <MainContent episodes={episodes} />
+    </>
+  );
 }
 
 export default App;
