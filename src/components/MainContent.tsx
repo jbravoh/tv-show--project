9c461ec9d5c5500ff3c9851e331c7256baeca914
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-23 12:58:03 +0100

    Add props to react component and map episodes

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,36 @@
+import Episode from "./Episode";
+import { IEpisode } from "./Episode";
+
+function MainContent(props: { episodes: IEpisode[] }): JSX.Element {
+  const showEpisodes = props.episodes.map((episode) => (
+    <Episode
+      key={episode.id}
+      id={episode.id}
+      url={episode.url}
+      name={episode.name}
+      season={episode.season}
+      number={episode.number}
+      airtime={episode.airtime}
+      airdate={episode.airdate}
+      airstamp={episode.airstamp}
+      runtime={episode.runtime}
+      type={episode.type}
+      image={{
+        medium: episode.image.medium,
+        original: episode.image.original,
+      }}
+      summary={episode.summary}
+      _links={episode._links}
+    />
+  ));
+
+  console.log(props);
+
+  return (
+    <main>
+      <section className="main-container">{showEpisodes}</section>
+    </main>
+  );
+}
+
+export default MainContent;
