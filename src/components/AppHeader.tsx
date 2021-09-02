1426985bc1e42eb8c578a7ca6321fae7dea3243f
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-09-02 14:13:23 +0100

    Change header title

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,7 +3,7 @@ import styles from "../css/AppHeader.module.css";
 function AppHeader(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
-      <h1 className={styles.title}> Game of Thrones</h1>
+      <h1 className={styles.title}> TV Shows</h1>
     </header>
   );
 }
