89502a99b277e368c5106a8f65f030d68d725bfd
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-09-02 14:15:11 +0100

    Add className and import css file

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,6 @@
 import { IEpisode } from "./Episode";
+import styles from "../css/Dropdown.module.css";
+
 interface DropdownProps {
   episodeData: IEpisode[];
   handleSelectEpisode: React.ChangeEventHandler<HTMLSelectElement>;
@@ -12,10 +14,18 @@ export default function Dropdown({
 }: DropdownProps): JSX.Element {
   return (
     <>
-      <select value={selectEpisode} onChange={handleSelectEpisode}>
+      <select
+        value={selectEpisode}
+        onChange={handleSelectEpisode}
+        className={styles.select}
+      >
         <option value={"default"}>Select an episode</option>
         {episodeData.map((episode) => (
-          <option key={episode.id} value={episode.name}>
+          <option
+            key={episode.id}
+            value={episode.name}
+            className={styles.option}
+          >
             S{String(episode.season).padStart(2, "0")}E
             {String(episode.number).padStart(2, "0")} - {episode.name}
           </option>
